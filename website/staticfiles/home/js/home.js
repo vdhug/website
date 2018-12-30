bcd16c1658a64802fe46d68e0403c299974aa408
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2018-12-29 22:35:53 -0200

    Adding transaction to home and about pages

---
+++ website/staticfiles/home/js/home.js
@@ -0,0 +1,25 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    var welcome = document.getElementById("welcome-div");
+    var home = document.getElementById("home-div");
+    var arrowUp = document.getElementById("arrow-up");
+    var arrowDown = document.getElementById("arrow-down");
+
+    setTimeout(function () {
+		welcome.classList.add("fadeInLeft");
+	}, 1000);
+
+    setTimeout(function () {
+		home.classList.add("fadeInDownBig");
+	}, 1500);
+
+    setTimeout(function () {
+		arrowUp.classList.add("fadeInUp");
+	}, 2500);
+
+    setTimeout(function () {
+		arrowDown.classList.add("fadeInDown");
+	}, 2500);
+
+});
