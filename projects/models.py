e208052adb5385405145dfc09ea8f80ce9e421df
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-02-01 15:47:21 -0200

    Adding models related to the backend of the projects module

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+	name = models.CharField(max_length=64)
+	description_project = models.TextField()
+	background_photo = models.ImageField(upload_to='projects')
+	description_company = models.CharField(max_length=80)
+	link_for_project_site = models.CharField(max_length=64)
+
+	def __str__(self):
+		return f"{self.name}: {self.description_company}"
+
+
+class Tech(models.Model):
+	name_icon = models.CharField(max_length=64)
+	title = models.CharField(max_length=64)
+	description = models.TextField()
+	project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='techs')
