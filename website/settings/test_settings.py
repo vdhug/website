c2ed220d44e394c842379c71b08d1f222a23ad67
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-02-05 15:59:47 -0200

    Excluding django_heroku management of static files

--- website/settings/test_settings.py
+++ website/settings/test_settings.py
@@ -118,8 +118,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+from website.aws.conf import *
