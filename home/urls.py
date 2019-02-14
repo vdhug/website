995ac02665514a3399da89555e37e550633cc381
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-02-14 16:31:59 -0200

    Adding translation of index page

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("english", views.english, name="english"),
+    path("spanish", views.spanish, name="spanish"),
+    path("portuguese", views.portuguese, name="portuguese")
 ]
